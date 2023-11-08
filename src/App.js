d42db185b34fd03efde48ec82fc9fba101442834
Sumit Kandari <sumitkandari8@gmail.com>
2023-11-08 09:29:47 +0530

    Router Changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import { Container } from "react-bootstrap";
 
-// import DisplayTodos from "./components/DisplayTodos";
 import { Provider } from "react-redux";
 import store from "./components/redux/store";
 import Addtodo from "./components/addTodo/Addtodo";
 import "./App.css";
 import Displaytodos from "./components/displaytodos/Displaytodos";
-import DisplayTodos from "./components/DisplayTodos";
-import AddTodo from "./components/AddTodo";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import { BrowserRouter, Route, Routes, Navigate,HashRouter } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Displayfavourite from "./components/favourite/Displayfavourite";
-import { useEffect } from "react";
-import axios from 'axios';
+
+
 
 function App() {
   
   return (
     <Provider store={store}>
-      {/* <Container className="py-3">
-      <AddTodo />
-      <DisplayTodos />
-    </Container> */}
-      <BrowserRouter>
+      
+      <HashRouter>
         <div className="app">
           <Navbar />
           <Routes>
@@ -38,7 +32,7 @@ function App() {
             <Route path="/favourite" element={<Displayfavourite />}/>
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
